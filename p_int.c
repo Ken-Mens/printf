5eaf43687be2cec074a27faf24d7f164d4a1db62
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-18 18:19:21 +0000

    update p_int

--- p_int.c
+++ p_int.c
@@ -1,14 +1,35 @@
 #include "holberton.h"
+/**
+ * p_int - prints the int
+ * @list: the argument passed through
+ * Return: the number of ints printed
+ */
 int p_int(va_list list)
 {
-	int x, i;
+
+	int n = list, counter = 0 ,temp, c = 1, spaces = 1, refill = 1;
+	int orig = n;
 	char *str;
 
-	x = va_arg(list, int);
-	str = convert(x, 10);
-	for (i = 0; str[i]; i++);
-	{
-		_putchar(str[i]);
+	n = va_arg(list, int);
+	if(n < 0)
+	{	
+		orig = (orig *-1);
+		_putchar('-');
+		counter++;
 	}
-	return (i);
+	temp = orig;
+	for (temp = orig; temp >= 10; temp /= 10)
+	spaces++;
+	for (; c < spaces; c++)
+	refill *= 10;
+	while(refill > 1)
+	{
+		_putchar((orig/refill) % 10 + '0');
+		refill /= 10
+		counter++;
+	}
+	_putchar(orig % 10 +'0');
+	counter++;
+	return (counter);
 }  
