21b7111269137430bf8fb0e8100e65e45d0ffb5f
Marc Cavigli <mcavigli@gmail.com>
2019-03-18 00:16:05 +0000

    Added comments

--- p_str.c
+++ p_str.c
@@ -1,4 +1,9 @@
 #include "holberton.h"
+/**
+ * p_str - prints the string
+ * @list: the argument passed through
+ * Return: the length of the string
+ */
 int p_str(va_list list)
 {
 	int length = 0;
